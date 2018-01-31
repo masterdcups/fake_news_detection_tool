5d898020d4928fa6a8bf8e9acad7a60064f1147d
Clément Bosc <clement.bosc@outlook.com>
2018-01-31 15:04:07 +0100

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.template import loader
+
+
+
+def index(request):
+    template = loader.get_template('app/index.html')
+    context = {
+        'latest_question_list': '',
+    }
+    return HttpResponse(template.render(context, request))
