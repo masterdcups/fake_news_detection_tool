6fc3e48e59953ac3365732f45faa9eb1e5647166
Clément Bosc <clement.bosc@outlook.com>
2018-04-12 15:11:21 +0200

    Factuality, opinion and emotion implementation

---
+++ app/criterias_calculation/emotion.py
@@ -0,0 +1,49 @@
+import nltk
+from statistics import mean
+from app.criterias_calculation.senticnet5 import senticnet
+
+
+class Emotion:
+
+    def __init__(self, text):
+        self.text = text
+
+    def get_score(article):
+        nltk.download('averaged_perceptron_tagger')
+
+        # version phrases
+        cpt_neg = 0
+        cpt_pos = 0
+        val_phrases = []
+
+        tokens = nltk.word_tokenize(article)
+        tagged_tokens = nltk.pos_tag(tokens)
+        for elem in tagged_tokens:
+            # print(elem)
+            if elem[0] == '.':
+                val_phrases.append((cpt_neg,cpt_pos))
+                cpt_pos = 0
+                cpt_neg = 0
+                # print(val_phrases)
+            elif elem[0] in senticnet:
+                # print(elem, senticnet[elem[0]])
+                if senticnet[elem[0]][6] == 'negative':
+                    cpt_neg += float(senticnet[elem[0]][7])
+                else:
+                    cpt_pos += float(senticnet[elem[0]][7])
+
+        cpt_neg = 0
+        cpt_pos = 0
+        for values in val_phrases:
+            cpt_neg += values[0]
+            cpt_pos += values[1]
+            # print(values, cpt_neg, cpt_pos)
+
+        nb_phrases = len(val_phrases)
+        cpt_neg = cpt_neg / nb_phrases
+        cpt_pos = cpt_pos / nb_phrases
+
+        score = abs(cpt_neg) + cpt_pos
+        print(score)
+
+        return score, abs(cpt_neg), cpt_pos
--- app/criterias_calculation/score_calculation.py
+++ app/criterias_calculation/score_calculation.py
@@ -1,4 +1,6 @@
 from app.criterias_calculation.controversy import Controversy
+from app.criterias_calculation.emotion import Emotion
+from app.criterias_calculation.factuality_opinion import FactualityOpinion
 from app.criterias_calculation.readability import Readability
 from app.criterias_calculation.score_normalization import ScoreNormalization
 from app.criterias_calculation.technicality import Technicality
@@ -16,13 +18,15 @@ class ScoreCalculation:
         controversy_score = Controversy.call(article)
         technicality_score = Technicality.get_score(article.text)
         trust_score, trust_confidence = Trust.call(url)
+        fact_score, opinion_score, fact_sents, opinion_sents, nb_sents = FactualityOpinion.classify(article.text)
+        emotion_score, nb_neg, nb_pos = Emotion.get_score(article.text)
 
         self.params = [
-            ['factuality', None, None, True],
+            ['factuality', fact_score, None, True],
             ['readability', readability_score,
              "Agreement rate : {}%".format(score_format(readability_taux_accord)), False],
-            ['emotion', None, None, True],
-            ['opinion', None, None, True],
+            ['emotion', emotion_score, None, True],
+            ['opinion', opinion_score, None, True],
             ['controversy', controversy_score, None, True],
             ['trust', score_format(trust_score),
              "Confidence score : {}%".format(score_format(trust_confidence)), False],
